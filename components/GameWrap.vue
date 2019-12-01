2bc5a9c220b5ec8799274229765e059cbdbef464
Sander Moolin <sandermoolin@gmail.com>
2019-12-01 13:26:35 -0500

    Some 3d boardwork

--- components/GameWrap.vue
+++ components/GameWrap.vue
@@ -1,6 +1,8 @@
 <template>
     <section class="game-wrap" v-if="ready">
-        <game-cell v-for="(cell, i) in G.grid" :key="i" :index="i" />
+        <div class="board">
+            <game-cell v-for="(cell, i) in G.grid" :key="i" :index="i" />
+        </div>
     </section>
 </template>
 
@@ -20,9 +22,25 @@ export default {
 
 <style lang="scss">
 .game-wrap {
-    --side: 45px;
-    display: grid;
-    grid-template: repeat(10, var(--side)) / repeat(10, var(--side));
-    margin: 40px;
+    --side: 75px;
+    --black: black;
+    --white: white;
+    text-align: center;
+
+    .board {
+        display: inline-grid;
+        grid-template: repeat(10, var(--side)) / repeat(10, var(--side));
+        grid-gap: 1px;
+        justify-content: center;
+
+        margin: 40px;
+        background: var(--black);
+        padding: 1px;
+
+        font-size: 14px;
+
+        --isometric: 55deg; //35.264deg;
+        transform: rotateX(var(--isometric)) rotateZ(45deg);
+    }
 }
 </style>
--- components/game/Cell.vue
+++ components/game/Cell.vue
@@ -1,16 +1,50 @@
 <template>
     <div class="game-cell">
-        {{ index }}
+        <div class="contents">
+            {{ contents }}
+        </div>
     </div>
 </template>
 
 <script>
+import { grid } from 'boardgame-utils'
+const { getCoordsFromIndex } = grid
+
 export default {
-    props: ['index']
+    props: ['index'],
+    computed: {
+        contents() {
+            const coords = getCoordsFromIndex(this.index, 10)
+            const val =
+                this.G.enemies.find(
+                    x => x.position.x == coords.x && x.position.y == coords.y
+                ) ||
+                this.G.players.find(
+                    x => x.position.x == coords.x && x.position.y == coords.y
+                )
+
+            // empty
+            if (!val) {
+                return val
+            }
+
+            return val.name
+        }
+    }
 }
 </script>
 
 <style lang="scss">
 .game-cell {
+    background: var(--white);
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    .contents {
+        // transform: rotateZ(-45deg);
+    }
 }
 </style>
